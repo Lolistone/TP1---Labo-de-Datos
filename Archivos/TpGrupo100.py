10b83e0b65c8a96984453c6bc6377f74a1e5aa84
Ramiro Padilla <rpadilla@dc.uba.ar>
2024-02-21 00:56:10 -0300

    Agregue ejiii)

--- Archivos/TpGrupo100.py
+++ Archivos/TpGrupo100.py
@@ -12,7 +12,6 @@ from inline_sql import sql, sql_val
 
 carpeta = "~/Dropbox/UBA/2024/LaboDeDatos/TP1/Tabla_Limp/TablasOriginales/" 
 
-
 # Creamos los Dataframes vacios.
 pais = pd.DataFrame(columns = ["idPais", "Nombre"])
 sede = pd.DataFrame(columns=["idSede","idPais","Descripcion","Estado"])
@@ -44,21 +43,25 @@ sedeDatosOriginal = sedeDatosOriginal.explode('redes_sociales')
 # Eliminamos las filas con red_social = ' ' creada sin intencion en el paso anterior
 sedeDatosOriginal = sedeDatosOriginal[sedeDatosOriginal['redes_sociales']!='']
 
+# Esto por si filtramos las redes que solo son URLS, (eliminamos los @ y cualquier otro texto) 
+# CONSERVAMOS los nulls?  si no, con na=False se eliminan tmbn, tambien podemos pasar de 'nan' a 'sinredsocial'
+# sedeDatosOriginal = sedeDatosOriginal[sedeDatosOriginal['redes_sociales'].str.contains('.com', case=False, na=True)]
+# AUNQUE LUEGO LO HICE EN LA CONSULTA SQL
+
 # Como el metodo explode() repite index para una misma fila, procedemos a reiniciar los mismos
 sedeDatosOriginal = sedeDatosOriginal.reset_index(drop=True)
 
 ### aca nombramos el problema de calidad que encontramos, no se cual es bien
 ### puede ser que hay muchas redes en una sola fila, lo q trae problemas de instancia?
-### 
+### Y no se respeta el atributo de calidad: Constancia.
 
 
 ### de PBIORIGINAL podemos decir q no se cumple el atributo completitud, faltan datos en 2022
 ### posible arreglo? eliminarlos 
+### Otro posible problema, es de relatividad, hay muchos "paises" que son regiones lo cual no es de interes
 
 
 # Importamos los datos (ya limpios) 
-
-# no entiendo bien como importar, esta bien asi?
 
 pais = sql^"""
             SELECT *
@@ -81,31 +84,31 @@ sede = sql^"""
             FROM sedeDatosOriginal AS sdo
             """
 
-# Aplicamos UNION (sin ALL) para eliminar tuplas duplicadas ###podemos eliminarla antes en la carga del .csv
+# Ya borramos duplicados antes
 redes = sql^"""
             SELECT *
             FROM redes
-            UNION 
+            UNION
             SELECT sdo.sede_id,
                     sdo.redes_sociales
             FROM sedeDatosOriginal AS sdo
             """
 
-# Aplicamos UNION (sin ALL) para eliminar tuplas duplicadas ya que existen varias sedes para un pais.
+# Aplicamos UNION (sin ALL) para eliminar tuplas duplicadas ya que existen varias sedes distintas en un pais.
 regiones = sql^"""
                 SELECT *
                 FROM regiones,
-                UNION 
+                UNION
                 SELECT sdo.pais_iso_3,
                         sdo.region_geografica
                 FROM sedeDatosOriginal AS sdo
                 """
 
-### en seccionesOriginal hay 3 duplicados
+### en seccionesOriginal hay 3 duplicados, por eso mismo UNION (SIN ALL)
 secciones = sql^"""
                 SELECT *
                 FROM secciones
-                UNION ALL
+                UNION
                 SELECT sco.sede_id, 
                         sco.sede_desc_castellano
                 FROM seccionesOriginal AS sco
@@ -119,13 +122,12 @@ pbi = sql^"""
                     pbo."2022" 
             FROM pbisOriginal AS pbo
             """
+ 
+#%% # # # # # # # # # # 
+# # # Ejercicio H # # # 
+# # # # # # # # # # # #
 
-
-
-
-
-# Ejercicios
-# 1)
+# i)
 
 #obtenemos pbi con nombre de cada pais
 consultaSQL = """
@@ -212,8 +214,63 @@ consultaSQL="""
             
 respuesta1=sql^ consultaSQL
 
+# ii)
 
+# El promedio no se ve afectado por valors NULL de PBI, creo por AVG() lo salva
+# Se puede agregar con WHERE p.PBI_2022 IS NOT NULL, pero es lo mismo
+paises_conAlmenos_1SedeyPBI = sql^ """
+                                SELECT DISTINCT s.idPais,
+                                                r.region,     
+                                                p.PBI_2022
+                                FROM sede AS s
+                                LEFT OUTER JOIN regiones AS r
+                                ON s.idPais = r.idPais
+                                LEFT OUTER JOIN pbi AS p    
+                                ON s.idPais = p.idPais                                                            
+                            """
 
+resultadoEJii = sql^"""
+                        SELECT Region AS "Región geográfica",
+                                COUNT(*) AS "Paises Con Sedes Argentinas",
+                                AVG(PBI_2022) AS "Promedio PBI per Cápita 2022 (U$S)" 
+                        FROM paises_conAlmenos_1Sede_ConPBI
+                        GROUP BY Region
+                        ORDER BY "Promedio PBI per Cápita 2022 (U$S)" DESC;
+                    """
 
+# iii)
 
+redes_segunPais = sql^"""
+                        SELECT r.*,
+                                s.idPais
+                        FROM redes AS r, sede AS s
+                        WHERE r.idSede = s.idSede
+                    """
+
+# No encontre otra manera que hacerlo MANUAL
+redes_Tipos = sql^"""
+                        SELECT DISTINCT r.idPais,
+                                CASE 
+                                   WHEN LOWER(r.URL) LIKE '%facebook%' THEN 'Facebook'
+                                   WHEN LOWER(r.URL) LIKE '%twitter%' THEN 'Twitter'
+                                   WHEN LOWER(r.URL) LIKE '%linkedin%' THEN 'LinkedIn'
+                                   WHEN LOWER(r.URL) LIKE '%instagram%' THEN 'Instagram'
+                                   WHEN LOWER(r.URL) LIKE '%youtube%' THEN 'Youtube'
+                                   WHEN LOWER(r.URL) LIKE '%flickr%' THEN 'Flickr'
+                                   ELSE 'SinRedSocial'
+                               END AS tipo_red
+                        FROM redes_segunPais AS r
+                    """
+
+# Solo resta contar, decidimos que los paies categorizados 'SinRedSocial' tengan 0 tipos redes
+resultadoEJiii = sql^"""
+                            SELECT rt.idPais,
+                                    COUNT(CASE 
+                                              WHEN (rt.tipo_red !='SinRedSocial') THEN 1 
+                                              ELSE NULL 
+                                          END
+                                         ) AS "Cantidad de tipos de red social"
+                            FROM redes_Tipos AS rt
+                            GROUP BY rt.idPais
+                        """
 
